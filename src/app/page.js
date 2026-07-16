dc185c0ef731f00cfd2173327531f3ef61eb20db
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-07-16 23:36:12 +0600

    Vacation update.

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <h1 className="text-4xl font-bold text-gray-800 dark:text-white">
+                Welcome to My App
+            </h1>
+        </div>
     );
 }
